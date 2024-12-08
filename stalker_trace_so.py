012eba370ceb652370c97031987cc9edb3714024
oacia <93508519+oacia@users.noreply.github.com>
2024-12-09 06:03:45 +0800

    Update stalker_trace_so.py
    1. 在ida中右键即可运行插件
    2. 可以在函数窗口中选择指定函数

--- stalker_trace_so.py
+++ stalker_trace_so.py
@@ -4,45 +4,206 @@ from idaapi import plugin_t
 from idaapi import PLUGIN_PROC
 from idaapi import PLUGIN_OK
 from idaapi import get_imagebase
+import idaapi
 import ida_nalt
 import idautils
 import idc
 import random
+from functools import reduce
+
+template_js = '''
+var func_addr = [func_addr];
+var func_name = [func_name];
+var so_name = "[so_name]";
+
+/*
+    @param print_stack: Whether printing stack info, default is false.
+*/
+var print_stack = false;
+
+/*
+    @param print_stack_mode
+    - FUZZY: print as much stack info as possible
+    - ACCURATE: print stack info as accurately as possible
+    - MANUAL: if printing the stack info in an error and causes exit, use this option to manually print the address
+*/
+var print_stack_mode = "FUZZY";
+
+function addr_in_so(addr){
+    var process_Obj_Module_Arr = Process.enumerateModules();
+    for(var i = 0; i < process_Obj_Module_Arr.length; i++) {
+        if(addr>process_Obj_Module_Arr[i].base && addr<process_Obj_Module_Arr[i].base.add(process_Obj_Module_Arr[i].size)){
+            console.log(addr.toString(16),"is in",process_Obj_Module_Arr[i].name,"offset: 0x"+(addr-process_Obj_Module_Arr[i].base).toString(16));
+        }
+    }
+}
+function hook_dlopen() {
+    Interceptor.attach(Module.findExportByName(null, "android_dlopen_ext"),
+        {
+            onEnter: function (args) {
+                var pathptr = args[0];
+                if (pathptr !== undefined && pathptr != null) {
+                    var path = ptr(pathptr).readCString();
+                    //console.log(path);
+                    if (path.indexOf(so_name) >= 0) {
+                        this.is_can_hook = true;
+                    }
+                }
+            },
+            onLeave: function (retval) {
+                if (this.is_can_hook) {
+                    // note: you can do any thing before or after stalker trace so.
+
+                    trace_so();
+                }
+            }
+        }
+    );
+}
+
+function trace_so(){
+    var times = 1;
+    var module = Process.getModuleByName(so_name);
+    var pid = Process.getCurrentThreadId();
+    console.log("start Stalker!");
+    Stalker.exclude({
+        "base": Process.getModuleByName("libc.so").base,
+        "size": Process.getModuleByName("libc.so").size
+    })
+    Stalker.follow(pid,{
+        events:{
+            call:false,
+            ret:false,
+            exec:false,
+            block:false,
+            compile:false
+        },
+        onReceive:function(events){
+        },
+        transform: function (iterator) {
+            var instruction = iterator.next();
+            do{
+                if (func_addr.indexOf(instruction.address - module.base) !== -1) {
+                    console.log("call" + times + ":" + func_name[func_addr.indexOf(instruction.address - module.base)])
+                    times = times + 1
+                    if (print_stack) {
+                        if (print_stack_mode === "FUZZY") {
+                            iterator.putCallout((context) => {
+                                console.log("backtrace:\n"+Thread.backtrace(context, Backtracer.FUZZY).map(DebugSymbol.fromAddress).join('\n'));
+                                console.log('---------------------')
+                            });
+                        }
+                        else if (print_stack_mode === "ACCURATE") {
+                            iterator.putCallout((context) => {
+                                console.log("backtrace:\n"+Thread.backtrace(context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join('\n'));
+                                console.log('---------------------')
+                            })
+                        }
+
+                        else if (print_stack_mode === "MANUAL") {
+                            iterator.putCallout((context) => {
+                                console.log("backtrace:")
+                                Thread.backtrace(context, Backtracer.FUZZY).map(addr_in_so);
+                                console.log('---------------------')
+                            })
+                        }
+                    }
+                }
+                iterator.keep();
+            } while ((instruction = iterator.next()) !== null);
+        },
+
+        onCallSummary:function(summary){
+
+        }
+    });
+    console.log("Stalker end!");
+}
+
+setImmediate(hook_dlopen,0);
+        '''
+
+
+class UI_Hook(idaapi.UI_Hooks):
+    def __init__(self):
+        idaapi.UI_Hooks.__init__(self)
+
+    def finish_populating_widget_popup(self, form, popup):
+        form_type = idaapi.get_widget_type(form)
+        if form_type == idaapi.BWN_FUNCS or form_type == idaapi.BWN_PSEUDOCODE or form_type == idaapi.BWN_DISASM:
+            idaapi.attach_action_to_popup(form, popup, "stalkerTraceSo:genJsScript", None)
+
+
+class GenerateFridaHookScript(idaapi.action_handler_t):
+    def __init__(self):
+        idaapi.action_handler_t.__init__(self)
+
+    def activate(self, ctx):
+
+        if ctx.widget_type == idaapi.BWN_FUNCS:
+            selected = [idaapi.getn_func(idx).start_ea for idx in ctx.chooser_selection]
+        else:
+            selected = idautils.Functions()
+
+        generate_js_script(selected)
+
+    def update(self, ctx):
+        return idaapi.AST_ENABLE_ALWAYS
+
+
+def generate_hook_code(template_js, func_addr, func_name, so_name):
+    replacements = {
+        "[func_addr]": ', '.join(func_addr),
+        "[func_name]": ', '.join(func_name),
+        "[so_name]": "%s" % so_name
+    }
+    return reduce(lambda acc, item: acc.replace(item[0], item[1]), replacements.items(), template_js)
+
+
+def generate_js_script(func_list):
+    func_addr = []
+    func_name = []
+    so_base = get_imagebase()
+    for func_ea in func_list:
+        # thumb mode
+        if idc.get_sreg(func_ea, "T"):
+            func_addr.append(hex(func_ea + 1 - so_base))
+        else:
+            func_addr.append(hex(func_ea - so_base))
+        func_name.append('"{}"'.format(idc.get_func_name(func_ea)))
+
+    so_path, so_name = os.path.split(ida_nalt.get_input_file_path())
+    hook_code = generate_hook_code(template_js, func_addr, func_name, so_name)
+    r = [random.choice("abcdefghijklmnopqrstuvwxyz") for _ in range(5)]
+    script_name = "trace_" + so_name.split(".")[0] + '_' + ''.join(r) + ".js"
+    save_path = os.path.join(so_path, script_name)
+    with open(save_path, "w", encoding="utf-8") as f:
+        f.write(hook_code)
+
+    print("usage:")
+    print(f'frida -U -l "{save_path}" -f [package name]')
+
 
 class stalker_trace_so(plugin_t):
     flags = PLUGIN_PROC
-    comment = "stalker_trace_so"
+    comment = "stalker trace so"
     help = ""
-    wanted_name = "stalker_trace_so"
+    wanted_name = "stalker trace so"
     wanted_hotkey = ""
 
     def init(self):
         print("stalker_trace_so plugin has been loaded.")
-        return PLUGIN_OK
+        idaapi.register_action(
+            idaapi.action_desc_t("stalkerTraceSo:genJsScript", "stalker trace so", GenerateFridaHookScript(), None,
+                                 None, 201))
+        # Add ui hook
+        self.ui_hook = UI_Hook()
+        self.ui_hook.hook()
+
+        return idaapi.PLUGIN_KEEP
 
     def run(self, arg):
-
-        func_addr = []
-        func_name = []
-        so_base = get_imagebase()
-        for func_ea in idautils.Functions():
-            # thumb mode
-            if idc.get_sreg(func_ea, "T"):
-                func_addr.append(hex(func_ea + 1-so_base))
-            else:
-                func_addr.append(hex(func_ea-so_base))
-            func_name.append('"{}"'.format(idc.get_func_name(func_ea)))
-        template_js = 'var func_addr = [{}];\nvar func_name = [{}];\nvar so_name = "{}";\n/*\n    @param print_stack\n    是否输出堆栈信息,默认为false\n*/\nvar print_stack = false;\n/*\n    @param print_stack_mode\n    - FUZZY: 打印尽可能多的堆栈信息\n    - ACCURATE: 打印尽可能准确的堆栈信息\n    - MANUAL: 如果打印堆栈报错退出, 使用该选项手动打印地址\n*/\n\nvar print_stack_mode = "FUZZY";\nfunction addr_in_so(addr){{\n    var process_Obj_Module_Arr = Process.enumerateModules();\n    for(var i = 0; i < process_Obj_Module_Arr.length; i++) {{\n        if(addr>process_Obj_Module_Arr[i].base && addr<process_Obj_Module_Arr[i].base.add(process_Obj_Module_Arr[i].size)){{\n            console.log(addr.toString(16),"is in",process_Obj_Module_Arr[i].name,"offset: 0x"+(addr-process_Obj_Module_Arr[i].base).toString(16));\n        }}\n    }}\n}}\nfunction hook_dlopen() {{\n    Interceptor.attach(Module.findExportByName(null, "android_dlopen_ext"),\n        {{\n            onEnter: function (args) {{\n                var pathptr = args[0];\n                if (pathptr !== undefined && pathptr != null) {{\n                    var path = ptr(pathptr).readCString();\n                    //console.log(path);\n                    if (path.indexOf(so_name) >= 0) {{\n                        this.is_can_hook = true;\n                    }}\n                }}\n            }},\n            onLeave: function (retval) {{\n                if (this.is_can_hook) {{\n                    //you can do any thing before stalker trace so\n                    \n                    trace_so();\n                }}\n            }}\n        }}\n    );\n}}\n\nfunction trace_so(){{\n    var times = 1;\n    var module = Process.getModuleByName(so_name);\n    var pid = Process.getCurrentThreadId();\n    console.log("start Stalker!");\n    Stalker.exclude({{\n        "base": Process.getModuleByName("libc.so").base,\n        "size": Process.getModuleByName("libc.so").size\n    }})\n    Stalker.follow(pid,{{\n        events:{{\n            call:false,\n            ret:false,\n            exec:false,\n            block:false,\n            compile:false\n        }},\n        onReceive:function(events){{\n        }},\n        transform: function (iterator) {{\n            var instruction = iterator.next();\n            do{{\n                if (func_addr.indexOf(instruction.address - module.base) !== -1) {{\n                    console.log("call" + times + ":" + func_name[func_addr.indexOf(instruction.address - module.base)])\n                    times = times + 1\n                    if (print_stack) {{\n                        if (print_stack_mode === "FUZZY") {{\n                            iterator.putCallout((context) => {{\n                                console.log("backtrace:\\n"+Thread.backtrace(context, Backtracer.FUZZY).map(DebugSymbol.fromAddress).join(\'\\n\'));\n                                console.log(\'---------------------\')\n                            }});\n                        }}\n                        else if (print_stack_mode === "ACCURATE") {{\n                            iterator.putCallout((context) => {{\n                                console.log("backtrace:\\n"+Thread.backtrace(context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).join(\'\\n\'));\n                                console.log(\'---------------------\')\n                            }})\n                        }}\n\n                        else if (print_stack_mode === "MANUAL") {{\n                            iterator.putCallout((context) => {{\n                                console.log("backtrace:")\n                                Thread.backtrace(context, Backtracer.FUZZY).map(addr_in_so);\n                                console.log(\'---------------------\')\n                            }})\n                        }}\n                    }}\n                }}\n                iterator.keep();\n            }} while ((instruction = iterator.next()) !== null);\n        }},\n\n        onCallSummary:function(summary){{\n\n        }}\n    }});\n    console.log("Stalker end!");\n}}\n\nsetImmediate(hook_dlopen,0);\n'
-        so_path, so_name = os.path.split(ida_nalt.get_input_file_path())
-        hook_code = template_js.format(', '.join(func_addr), ', '.join(func_name), so_name)
-        r = [random.choice("abcdefghijklmnopqrstuvwxyz") for _ in range(5)]
-        script_name = "trace_" + so_name.split(".")[0] +'_'+''.join(r) + ".js"
-        save_path = os.path.join(so_path, script_name)
-        with open(save_path, "w", encoding="utf-8") as f:
-            f.write(hook_code)
-
-        print("usage:")
-        print(f'frida -U -l "{save_path}" -f [package name]')
+        generate_js_script(idautils.Functions())
 
     def term(self):
         pass
